18b1d03527a89be2c4c1e5cac4c9b8fc3de14c9c
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-08-17 01:07:29 -0300

    scopes

--- main2.js
+++ main2.js
@@ -1,3 +1,14 @@
 const a = 1
 
 console.log('IIFE', a)
+
+if (a === 1) {
+  console.log('teste')
+} else {
+  console.log('testando')
+}
+/*
+if, switch -> condicionais -> nao gera valor
+
+for, whie, do while -> estruturas de repetição
+*/
