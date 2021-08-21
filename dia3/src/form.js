bb4e9ddc31d9c754a5cc7b1532c4efc1fc5160b9
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-08-20 23:33:42 -0300

    day3 init

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -23,4 +23,47 @@ function fix(word) {
   return`${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`
 };
 
+/* ex 02
+  Adicione ao formulário um select do tipo multiple com algumas cores para seleção.
+*/
+function $ (selector) {
+  return document.querySelector(`[data-js="${selector}"]`);
+};
+// $ e _ também pode se usar como nome de váriavel
+const form = $('form');
+const select = document.createElement('select');
+const colors = ['#836FFF', '#DC143C', '#00CED1', '#8A2BE2', '#FFB6C1'];
+const colorBox = document.createElement('div');
+colorBox.style.display = 'flex'
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  select.appendChild(option)
+});
 
+select.addEventListener('change', (event) => {
+  // limpando e passando novas cores
+  colorBox.innerHTML = ''
+
+  Array.from(event.target.selectedOptions).forEach(option => {
+   // const div = document.createElement('div')
+   div.style.width = '100px';
+   div.style.height = '100px';
+   div.style.background = option.value;
+   colorBox.appendChild(div)
+  });
+});
+
+function createDiv(value) {
+  const div = document.createElement('div')
+  div.style.width = '100px';
+  div.style.height = '100px';
+  div.style.background = value;
+  colorBox.appendChild(div)
+  return div
+}
+
+select.setAttribute('multiple', '');
+form.appendChild(select);
+document.body.appendChild(colorBox)
