aa99d9195705c212a3475e972be57ed6f68e2ee1
Giovanna Linda <eugiovannasouza@gmail.com>
2021-08-22 01:07:05 -0300

    ex 02

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 
+const url = 'http://localhost:3333/cars'
 const form = document.querySelector('[data-js="form-cars"]')
 const table = document.querySelector('[data-js="table"]')
 
@@ -17,17 +18,71 @@ form.addEventListener('submit', (event) => {
   const plate = getElement('plate')
   const color = getElement('color')
 
-  const elements = [image, model, year, plate, color]
+  const data = {
+    image: image.value,
+    model: model.value,
+    year: year.value,
+    plate: plate.value,
+    color: color.value
+  }
 
+  createRow(data)
+
+  event.target.reset()
+  image.focus()
+});
+
+function createRow(data) {
   const tr = document.createElement('tr')
+
+  const elements = [
+    { type: 'image', value: data.image },
+    { type: 'model', value: data.model },
+    { type: 'year', value: data.year },
+    { type: 'plate', value: data.plate },
+    { type: 'color', value: data.color }
+  ]
+
   elements.forEach(element => {
     const td = document.createElement('td')
     td.textContent = element.value
     tr.appendChild(td)
   })
-  table.appendChild(tr)
 
-  event.target.reset()
-  image.focus()
-});
+  table.appendChild(tr)
+}
+
+  function createNoCar() {
+    const tr = document.createElement('tr')
+    const td = document.createElement('td')
+    const th = document.querySelectorAll('table th')
+    td.setAttribute('colspan', th.length)
+    td.textContent = 'Nenhum carro encontrado!'
+    tr.appendChild(td)
+    table.appendChild(tr)
+  }
+
+async function main() {
+  // pegar o erro
+  const result = await fetch(url)
+  .then(r => r.json())
+  .catch(error => ({ error: true, message: error.message}))
+
+  if(result.error) {
+    console.log('Erro ao buscar carros', result.message)
+    return
+  }
+
+  if(result.length === 0) {
+    createNoCar()
+    return
+  }
+  // sucesso
+  result.forEach(car => {
+  // funçao que cria uma linha na tabela para cada carro
+    createRow(car)
+  })
+}
+
+main()
 
